7daab2db0b1d8288c4ff25c67b1fd3e659ed5bda
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-10-06 13:35:37 -0400

    Debugging Tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -36,6 +36,13 @@ it("loads data, books an interview and reduces the spots remaining for the first
 
   fireEvent.click(getByText(appointment, "Save"));
 
-  //console.log(prettyDOM(appointment));
+  expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+  await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+  const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
+
+  expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+
 
 });
